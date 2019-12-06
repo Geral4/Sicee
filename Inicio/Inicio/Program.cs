7d7ab3f85c65ce4dca351df181184462fb3d5134
gerald.r.g@hotmail.com <gerald.r.g@hotmail.com>
2019-12-05 22:13:54 -0600

    CorreccionDetalle

--- Inicio/Inicio/Program.cs
+++ Inicio/Inicio/Program.cs
@@ -16,7 +16,7 @@ namespace Inicio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Inicio());
+            Application.Run(new Login());
         }
     }
 }
